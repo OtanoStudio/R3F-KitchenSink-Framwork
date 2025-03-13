6c316689feb76cb30e6ff6c44ea55a2117841031
otanodesignco <otanodesign@gmail.com>
2025-03-13 14:26:17 -0500

    added noise based halftone and webp noise

--- src/shaders/lib/color/colorContrast.glsl
+++ src/shaders/lib/color/colorContrast.glsl
@@ -6,3 +6,15 @@ vec3 colorContrast( vec3 color, float contrast )
     return ( color - midPoint ) * contrast + midPoint;
     
 }
+
+vec3 colorContrast( vec3 color, float contrast, float threshold ) 
+{
+    float gray = dot( color, vec3( 0.299, 0.587, 0.114 ) );
+    
+    float adjustedGray = threshold + ( gray - threshold ) * contrast;
+    
+    adjustedGray = clamp( adjustedGray, 0.0, 1.0 );
+    
+    return vec3(adjustedGray);
+
+}
