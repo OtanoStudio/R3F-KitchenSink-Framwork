70fb47dc28d5f9e53ec73d5aa04b910bb5dbc562
otanodesignco <otanodesign@gmail.com>
2025-03-20 15:32:05 -0500

    fixed blur and added camera distance utility function

--- src/shaders/lib/blur/blurRadial.glsl
+++ src/shaders/lib/blur/blurRadial.glsl
@@ -1,4 +1,4 @@
-vec4 blurRadial(sampler2D image, vec2 uv, vec2 center, float radius, int samples) 
+vec4 blurRadial( sampler2D image, vec2 uv, vec2 center, float radius, int samples ) 
 {
 
     vec4 color = vec4(0.0);
@@ -15,4 +15,24 @@ vec4 blurRadial(sampler2D image, vec2 uv, vec2 center, float radius, int samples
     
     return color / float(samples);
     
+}
+
+vec4 blurRadical( sampler2D img, vec2 uv, vec2 center, float radius, int samples, float amount )
+{
+
+    vec4 blurImg = vec4( 0.0 );
+    vec2 dist = uv - center;
+
+    int j = 0;
+
+    for( j = 0; j < samples, j++ )
+    {
+        float scale = 1.0 - amount * ( float( j ) / float( samples ) ) * ( clamp( length( dist ) / radius , 0.0, 1.0 ) );
+        blurImg += texture( img, dist * scale + center );
+    }
+
+    blurImg /= float( samples );
+
+    return blurImg;
+
 }
