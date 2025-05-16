6fb912b2e5f8d3d36b4d8525b55cea0cd85b1ad3
otanodesignco <otanodesign@gmail.com>
2025-05-15 23:27:06 -0500

    added smoothstep utilNoiseOutline that has not been tested yet

--- src/shaders/lib/util/utilNoiseOutline.glsl
+++ src/shaders/lib/util/utilNoiseOutline.glsl
@@ -6,3 +6,13 @@ float utilNoiseOutline(
 {
     return step( threshold -  width, noise ) - step( threshold + width, noise );
 }
+
+float utilNoiseOutline(
+    float noise, // noise that has not had a smoothstep or step applied
+    float start, // start for the smoothstep
+    float end, // end for the smoothstep
+    float width // width of the outline
+)
+{
+    return smoothstep( start -  width, end, noise ) - smoothstep( start + width, end, noise );
+}
