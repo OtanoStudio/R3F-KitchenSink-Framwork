0974e2306e99f83850f31167c50bbd624d633dc0
otanodesignco <otanodesign@gmail.com>
2025-05-16 03:40:20 -0500

    combined  utilNoiseOutline smoothstep and step into 1 function

--- src/shaders/lib/util/utilNoiseOutline.glsl
+++ src/shaders/lib/util/utilNoiseOutline.glsl
@@ -1,17 +1,26 @@
 float utilNoiseOutline(
     float noise, // noise that has not had a smoothstep or step applied
     float threshold, // threshold for the shaping function
-    float width // width of the outline
+    float width, // width of the outline
+    int type // 0 for step, 1 for smoothstep
 )
 {
-    return step( threshold -  width, noise ) - step( threshold + width, noise );
-}
 
-float utilNoiseOutline(
-    float noise, // noise that has not had a smoothstep or step applied
-    float threshold, // threshold for the shaping function
-    float width // width of the outline
-)
-{
-    return smoothstep( threshold -  width, threshold, noise ) - smoothstep( threshold, threshold + width, noise );
+    float rtn = 0.0;
+
+    if( type == 0 )
+    {
+
+        rtn = step( threshold -  width, noise ) - step( threshold + width, noise );
+    
+    }
+    else // we don't care if type is anything but 0 it will be smoothstep
+    {
+
+        rtn = smoothstep( threshold -  width, threshold, noise ) - smoothstep( threshold, threshold + width, noise );
+    
+    }
+
+    return rtn;
+
 }
