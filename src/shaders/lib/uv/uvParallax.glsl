b665caa51deebdfa40d031d5d1b224e773ef483e
otanodesignco <otanodesign@gmail.com>
2025-05-22 14:17:30 -0500

    added more uvParallax methods

--- src/shaders/lib/uv/uvParallax.glsl
+++ src/shaders/lib/uv/uvParallax.glsl
@@ -20,9 +20,60 @@ vec2 uvParallax(
 
 vec2 uvParallax( 
     vec2 uv, // uv
+    sampler2D height, // texture for depth
+    float depthOffset, // depth offset
+    float offset // offset 
+)
+{
+
+    float depth = texture( height, uv ).r;
+    vec2 rtn = uv - 0.5;
+    rtn *= ( depth * depthOffset );
+
+    return rtn + offset;
+
+}
+
+vec2 uvParallax( 
+    vec2 uv, // uv
     float height, // generated height
     float offset // offset for the depth
 )
 {
     return uv + ( height * offset );
+}
+
+vec2 uvParallax( 
+    vec2 uv, // uv
+    float height, // texture for depth
+    float depthOffset, // depth offset
+    float offset // offset 
+)
+{
+    
+    vec2 rtn = uv - 0.5;
+    rtn *= ( height * depthOffset );
+
+    return rtn + offset;
+
+}
+
+// these scroll the depth texture
+vec2 uvParallax( 
+    vec2 uv, // uv
+    sampler2D height, // texture for depth
+    float depthOffset, // depth offset
+    float offset, // offset
+    vec2 time, // fixed time with scroll speed included
+
+)
+{
+    vec2 scroll = uv + time;
+
+    float depth = texture( height, scroll ).r;
+    vec2 rtn = uv - 0.5;
+    rtn *= ( depth * depthOffset );
+
+    return rtn + offset;
+
 }
