2d4e6a17c4ececea18b6f7d9d78ee2749edfeb18
otanodesignco <otanodesign@gmail.com>
2025-07-21 16:48:12 -0500

    added simple phong anisotropic

--- src/shaders/lib/BRDF/anisotropicPhong.glsl
+++ src/shaders/lib/BRDF/anisotropicPhong.glsl
@@ -25,3 +25,29 @@ float anisotropicPhong(
     return a;
     
 }
+
+float anisotropicSimplePhong(
+    vec3 n,
+    vec3 l,
+    vec3 v,
+    vec3 t,
+    vec3 b,
+    float au,
+    float av
+)
+{
+
+    float PI = 3.14159265358979323846;
+
+    vec3 h = normalize( l + v );
+    float ndoth = max( dot( n, h ), 0.001 );
+    float hdott = dot( h, t );
+    float hdotb = dot( h, b );
+
+    float exponent = au * pow( hdott, 2.0 ) + av * pow( hdotb, 2.0 );
+
+    float specular = pow( ndoth, exponent );
+
+    return specular;
+
+}
