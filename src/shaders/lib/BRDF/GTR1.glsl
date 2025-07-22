48b0c76b84577bb661661e608cf4035deaa521fc
otanodesignco <otanodesign@gmail.com>
2025-07-21 19:37:27 -0500

    added gtr1 distribution

---
+++ src/shaders/lib/BRDF/GTR1.glsl
@@ -0,0 +1,13 @@
+float GTR1(
+    float ndoth, 
+    float gloss
+)
+{
+    float PI = 3.14159265358979323846;
+
+    float a = mix( 0.1, 0.001, gloss );
+    float a2 = a * a;
+    float denom = PI * log( a2 ) * ( 1.0 + ( a2 - 1.0 ) * ndoth * ndoth );
+    return a2 / denom;
+
+}
