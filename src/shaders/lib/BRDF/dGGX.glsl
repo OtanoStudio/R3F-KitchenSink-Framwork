b5ab1df24a8af13703e305f55918ebc8c798f0be
otanodesignco <otanodesign@gmail.com>
2025-07-21 19:28:34 -0500

    added ggx distribution

---
+++ src/shaders/lib/BRDF/dGGX.glsl
@@ -0,0 +1,14 @@
+float dGGX(
+    float NdotH, 
+    float roughness
+) 
+{
+
+    float PI = 3.14159265358979323846;
+
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float denom = NdotH * NdotH * ( a2 - 1.0 ) + 1.0;
+    return a2 / ( PI * denom * denom );
+
+}
