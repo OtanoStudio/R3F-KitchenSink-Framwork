51d08d7eceaa2253f61527f29e10911383805f61
otanodesignco <otanodesign@gmail.com>
2025-08-02 05:27:01 -0500

    fixed wireframe code

--- src/components/Wired.jsx
+++ src/components/Wired.jsx
@@ -1,3 +1,4 @@
+import { calculateBaryCoords } from "../calculateBaryCoords.js";
 import WireframeMaterial from "../materials/meshWireframeMaterial.jsx"
 import { IcosahedronGeometry, BufferAttribute, DoubleSide  } from "three"
 
@@ -5,30 +6,22 @@ export default function Wired()
 {
     const geo = new IcosahedronGeometry( 1, 1 );
 
-    const geoCount = geo.attributes.position.array.length;
+    calculateBaryCoords( geo );
 
-    let b = [];
-    
 
-    for( let i = 0; i < geoCount / 3; i++ )
-    {
-        b.push( 0, 0, 1, 0, 1, 0, 1, 0, 0 );
-    }
-    
-
-    const bary = new Float32Array( b );
-
-    geo.setAttribute( 'baryCoords', new BufferAttribute( bary, 3 ) );
 
   return (
     <mesh geometry={ geo }>
         <WireframeMaterial
-            lineWidth={ 1.3 } 
+            lineWidth={ 3 }
             gradient={ true }
+            color={ '#ca457b'} 
+            backColor={ '#9fa1df' }
             gradientTop={ '#9fa1df' }
             gradientBottom={ '#ca457b' }
             brightness={ 2 }
-            seeThrough={ true}
+            seeThrough={ true }
+            colorBack={ true }
         />
     </mesh>
   )
--- src/materials/meshWireframeMaterial.jsx
+++ src/materials/meshWireframeMaterial.jsx
@@ -11,27 +11,42 @@ export default function WireframeMaterial({
     gradientBottom = '#2e6659',
     gradient = false, // toggle gradient
     brightness = 1.0, // brightness for colors
+    colorBack = false, // enable double side color
     seeThrough = false, // determine if material is see-through
     backColor = '#ffffff', // if backface enabled set backside color
+    backGradientTop = '#c3a867',
+    backGradientBottom = '#2e6659',
 }) 
 {
     color = new Color( color );
+    color.multiplyScalar( brightness );
+    backColor = new Color( backColor );
+    backColor.multiplyScalar( brightness );
     gradientTop = new Color( gradientTop );
     gradientTop.multiplyScalar( brightness );
     gradientBottom = new Color( gradientBottom );
     gradientBottom.multiplyScalar( brightness );
+    backGradientTop = new Color( backGradientTop );
+    backGradientTop.multiplyScalar( brightness );
+    backGradientBottom = new Color( backGradientBottom );
+    backGradientBottom.multiplyScalar( brightness );
 
     gradient = typeof gradient === 'boolean' ? gradient : false;
     seeThrough = typeof seeThrough === 'boolean' ? seeThrough : false;
+    colorBack = typeof colorBack === 'boolean' ? colorBack : false;
 
     const uniforms =
     {
         uLineWidth: lineWidth,
         uBaseColor: color,
+        uBackColor: backColor,
         uGradientTop: gradientTop,
         uGradientBottom: gradientBottom,
         uGradient: gradient,
         uBackground: seeThrough,
+        uGradientBackTop: backGradientTop,
+        uGradientBackBottom: backGradientBottom,
+        uBackColored: colorBack,
     };
 
     const WireframeMaterial = shaderMaterial( uniforms, vertex, fragment );
