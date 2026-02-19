ef55d5c95b7be3a60154a2aa75a6166459183e74
Otano Studio <otanodesign@gmail.com>
2026-02-19 17:58:26 -0600

    fixed wind functions & added gust, phase, and stiffness controls

--- src/shaders/lib/util/wind.glsl
+++ src/shaders/lib/util/wind.glsl
@@ -1,28 +1,144 @@
 // utility function for creating wind using a noise texture and animation
 
-
 vec3 wind(
     sampler2D noiseTexture, // noise texture image
-    vec3 position, // world space position
-    vec2 positionOffset, // position offset of the wind as a vec2
+    vec2 windUV, // world space position
+    vec3 positionOffset, // position offset of the wind as a vec2
     vec2 speed, // vec2( time ) * timingOffset
-    vec2 windVelocity, // speed of the wind as a vec2
+    vec3 windVelocity, // speed of the wind as a vec2
     vec2 uv, // texture coordinates
-    float windOffset, // offset of the wind effect
+    vec2 windOffset, // offset of the wind effect x overall multiplier y stiffness power
+
 
 )
 {
 
-    float windNoise = texture( noiseTexture, ( position.xz * positionOffset ) - ( speed * windVelocity ) ).r;
-    float windAffect = pow( 1.0 - uv.y, 2.0 );
+    vec2 scroll = speed * windVelocity;
+    windUV *= positionOffset.xy;
+    float windNoise = texture( noiseTexture, windUV  - scroll ).r * 2.0 - 1.0;
+    float windAffect = 1.0 - uv.y;
+    windAffect *= windOffset.y;
 
+    vec2 windDirection = normalize( windVelocity.xy );
     vec3 rtn = vec3(
-        windNoise * windVelocity.x,
-        0.0,
-        windNoise * windVelocity.y
-    ) * windOffset;
+        windNoise * windDirection.x,
+        windNoise * positionOffset.z,
+        windNoise * windDirection.y
+    ) * windOffset.x;
 
-    rtn *= windAffect;
+    rtn *= windAffect * windVelocity.z;
+
+    return rtn;
+
+}
+
+vec3 wind(
+    sampler2D noiseTexture, // noise texture image
+    vec2 windUV, // world space position
+    vec3 positionOffset, // position offset of the wind as a vec2
+    vec2 speed, // vec2( time ) * timingOffset
+    vec3 windVelocity, // speed of the wind as a vec2
+    vec2 uv, // texture coordinates
+    vec2 windOffset, // offset of the wind effect
+    float phase, // phase offset to break up uniformity
+
+)
+{
+
+    vec2 scroll = speed * windVelocity.xy;
+    windUV *= positionOffset.xy;
+    float windNoise = texture( noiseTexture, windUV  - scroll + phase ).r * 2.0 - 1.0;
+    float windAffect = 1.0 - uv.y;
+    windAffect *= windOffset.y;
+
+    vec2 windDirection = normalize( windVelocity.xy );
+    vec3 rtn = vec3(
+        windNoise * windDirection.x,
+        windNoise * positionOffset.z,
+        windNoise * windDirection.y
+    ) * windOffset.x;
+
+    rtn *= windAffect * windVelocity.z;
+
+    return rtn;
+
+}
+
+vec3 wind(
+    sampler2D noiseTexture, // noise texture image
+    vec2 windUV, // world space position
+    vec3 positionOffset, // position offset of the wind as a vec2
+    vec2 speed, // vec2( time ) * timingOffset
+    vec3 windVelocity, // speed of the wind as a vec2
+    vec2 uv, // texture coordinates
+    vec2 windOffset, // offset of the wind effect
+    vec3 gustOffset, // gust offset for texture sampling x and  with z being the blend factor
+
+)
+{
+
+    vec2 scroll = speed * windVelocity.xy;
+    windUV *= positionOffset.xy;
+    float windNoise = texture( noiseTexture, windUV  - scroll ).r * 2.0 - 1.0;
+
+    scroll *= gustOffset.y;
+    windUV *= gustOffset.x;
+
+    float windNoise2 = texture( noiseTexture, windUV - scroll ).g;
+    float windFinal = mix( windNoise, windNoise2, gustOffset.z );
+
+    float windAffect = 1.0 - uv.y;
+    windAffect *= windOffset.y;
+
+    vec2 windDirection = normalize( windVelocity.xy );
+    vec3 rtn = vec3(
+        windFinal * windDirection.x,
+        windFinal * positionOffset.z,
+        windFinal * windDirection.y
+    ) * windOffset.x;
+
+    rtn *= windAffect * windVelocity.z;
+
+    return rtn;
+
+}
+
+// wind with phase and turbulence/gusts
+vec3 wind(
+    sampler2D noiseTexture, // noise texture image
+    vec2 windUV, // world space position xz
+    vec3 positionOffset, // position offset of the wind as a vec3, z is the vertical lift
+    vec2 speed, // vec2( time ) * timingOffset
+    vec3 windVelocity, // speed of the wind as a vec3 z is the strength modifier
+    vec2 uv, // texture coordinates
+    vec2 windOffset, // offset of the wind effect
+    float phase, // phase offset to break up uniformity
+    vec3 gustOffset, // gust offset for texture sampling x and  with z being the blend factor
+
+)
+{
+
+    vec2 scroll = speed * windVelocity.xy;
+    windUV *= positionOffset.xy;
+    float windNoise = texture( noiseTexture, windUV  - scroll + phase ).r * 2.0 - 1.0;
+    
+    scroll *= gustOffset.y;
+    windUV *= gustOffset.x;
+    float windNoise2 = texture( noiseTexture, windUV - scroll ).g;
+
+    float windFinal = mix( windNoise, windNoise2, gustOffset.z );
+
+    float windAffect = 1.0 - uv.y;
+    windAffect *= windOffset.y;
+
+    vec2 windDirection = normalize( windVelocity.xy );
+    vec3 rtn = vec3(
+        windFinal * windDirection.x,
+        windFinal * positionOffset.z,
+        windFinal * windDirection.y
+    ) * windOffset.x;
+
+    rtn *= windAffect * windVelocity.z;
 
     return rtn;
 
