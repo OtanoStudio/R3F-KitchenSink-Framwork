5cca8764888047ffa89e8b50b0372204de49251a
Otano Studio <otanodesign@gmail.com>
2026-05-10 16:08:01 -0500

    fixed mouse hooks, added lerp from prev position for better smoothing

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -10,6 +10,7 @@ import gsap from "gsap"
 import { useEffect, useMemo } from "react"
 import ParallaxDepth from "./components/ParallaxDepth.jsx"
 //import GridLightColumns from "./components/GridLightColumns"
+import { CyborgPlane } from './components/CyborgPlane.jsx'
 
 
 export default function Experience()
@@ -45,16 +46,17 @@ export default function Experience()
             <DigitizePlane position={[ 0, 0, -0.08 ]} digaDensity={ new Vector3( 0.1, 0.05, 0.025 )} /> */}
             {/* <DigitalTrianglePortal /> */}
             
-            <GrassField />
+            {/* <GrassField />
             <mesh 
                 rotation-x={ -90 * Math.PI / 180 }
                 position-y={ -0.49 }
             >
                 <planeGeometry args={[10, 10]} />
                 <meshBasicMaterial color='#5e3958' />
-            </mesh>
+            </mesh> */}
 
             {/* <ParallaxDepth /> */}
+            <CyborgPlane />
         </group>
     
     )
