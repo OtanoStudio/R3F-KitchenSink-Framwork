16812ccd329e3799b68b0b4e2f244c49a27a1f7e
Otano Studio <otanodesign@gmail.com>
2026-06-28 05:57:03 -0500

    added Beer-Powder function

---
+++ src/shaders/lib/lighting/lightingBeerPowder.glsl
@@ -0,0 +1,12 @@
+lightingBeerPowder(
+    float density,
+    float absorption
+)
+{
+
+    float beer = exp( -density * absorption );
+    float powder = 1.0 - exp( -density * absorption );
+
+    return beer * powder;
+
+}
