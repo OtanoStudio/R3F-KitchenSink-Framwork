a56b5120a1efa39a6f50d10d30ac41263691ef3c
Otano Studio <otanodesign@gmail.com>
2026-07-26 15:39:50 -0500

    added lighting beer law vec3 function

--- src/shaders/lib/lighting/lightingHGPhase.glsl
+++ src/shaders/lib/lighting/lightingHGPhase.glsl
@@ -6,6 +6,7 @@ float lightingHGPhase(
 
     float scatter2 = scatter * scatter;
     float denom = 1.0 + scatter2 - 2.0 * scatter * lightAngle;
+    denom = max( denom, 0.0001 );
 
     return ( 1.0 - scatter2 ) / ( 4.0 * 3.14159265 * denom * sqrt( denom ) );
 
